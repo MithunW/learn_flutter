9aa0eef60176e6dfe791a467f85300211d1815a8
MithunW <mithun.17@cse.mrt.ac.lk>
2019-10-22 13:30:05 +0530

    error fix test

--- flutter_project/lib/myapp.dart
+++ flutter_project/lib/myapp.dart
@@ -1,60 +1,126 @@
-import 'package:english_words/english_words.dart';
-import 'package:english_words/english_words.dart' as prefix0;
-import 'package:flutter/cupertino.dart';
+// import 'package:english_words/english_words.dart';
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+
+// class MyApp extends StatelessWidget {
+//   Widget build(BuildContext context) {
+//     return MaterialApp(title: "Startup Name Generator", home: RandomWords());
+//   }
+// }
+
+// class RandomWordState extends State<RandomWords> {
+//   final _suggestions = <WordPair>[];
+//   final _biggerFont = const TextStyle(fontSize: 18.0);
+
+//   Widget _buildSuggestions() {
+//     return ListView.builder(
+//         padding: EdgeInsets.all(16.0),
+//         itemBuilder: (context, i) {
+//           if (i.isOdd) return Divider(color: Colors.red);
+//           final index = i ~/ 2;
+//           if (index >= _suggestions.length) {
+//             _suggestions.addAll(generateWordPairs().take(10));
+//           }
+//           return _buildRow(_suggestions[index]);
+//         });
+//   }
+
+//   Widget _buildRow(WordPair pair) {
+//     title:
+//     Text(
+//       pair.asPascalCase,
+//       style: _biggerFont,
+//     );
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Startup Name Generator'),
+//       ),
+//       body: _buildSuggestions(),
+//     );
+//   }
+// }
+
+// class RandomWords extends StatefulWidget {
+//   RandomWordState createState() => RandomWordState();
+// }
+
+
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
-class MyApp extends StatelessWidget{
-  Widget build(BuildContext context){
-   final wordPair=WordPair.random();
+// void main() => runApp(MyApp());
+
+// #docregion MyApp
+class MyApp extends StatelessWidget {
+  // #docregion build
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to flutter',
-      home: Scaffold(
-        appBar:AppBar(
-          title:Text('Welcome to Flutter') ,
-          backgroundColor:Colors.orangeAccent,
-          ) ,
-          body: Center(
-            child: RandomWords(),
-            ),
-            backgroundColor: Colors.orangeAccent[100],
-            ),
+      title: 'Startup Name Generator',
+      home: RandomWords(),
     );
   }
+  // #enddocregion build
 }
+// #enddocregion MyApp
 
-class RandomWordState extends State<RandomWords>{
-  final _suggestions=<WordPair>[];
-  final _biggerFont=const TextStyle(fontSize:18.0);
-  Widget _buildSuggestions(){
+// #docregion RWS-var
+class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+  // #enddocregion RWS-var
+
+  // #docregion _buildSuggestions
+  Widget _buildSuggestions() {
     return ListView.builder(
-      padding: EdgeInsets.all(16.0),
-      itemBuilder: (context,i){
-        if (i.isOdd) return Divider(color: Colors.red);
-        final index=i~/2;
-        if (index>=_suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: /*1*/ (context, i) {
+          if (i.isOdd) return Divider(); /*2*/
 
-        }
-        return _buildRow(_suggestions[index]);
-
-        
-      }
-
-    );
-    
+          final index = i ~/ 2; /*3*/
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+          }
+          return _buildRow(_suggestions[index]);
+        });
   }
+  // #enddocregion _buildSuggestions
 
+  // #docregion _buildRow
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+  // #enddocregion _buildRow
 
+  // #docregion RWS-build
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+  // #enddocregion RWS-build
+  // #docregion RWS-var
 }
+// #enddocregion RWS-var
 
-class RandomWords extends StatefulWidget{
-  RandomWordState createState()=>RandomWordState();
-  Widget _buildRow(WordPair pair){
-    title:Text(
-      pair.asPascalCase,
-      style: _biggerFont
-    ),
-
-  );
-}
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
 }
--- flutter_project/test/widget_test.dart
+++ flutter_project/test/widget_test.dart
@@ -6,10 +6,9 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_project/myapp.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_project/main.dart';
-import 'package:/flutter_project/myapp.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
